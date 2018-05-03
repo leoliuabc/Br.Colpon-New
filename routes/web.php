5ed1cb509a526f772d398da61e1b52aff3993482
Leo <leoliuabc@gmail.com>
2018-05-03 15:49:12 +0800

    Routes

--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,26 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// User Auth
 Auth::routes();
 
+// Home
 Route::get('/', 'HomeController@index');
-Route::get('/lojas/{titleslug}/{id}', 'StoreController@index');
-Route::get('/lojas', 'StoreController@map');
-Route::get('/cupons/{store_id}/{id}', 'OfferController@index');
-Route::get('/cupons', 'OfferController@new_offers');
+
+// Lojas
+Route::group(['prefix' => 'lojas'], function(){
+    Route::get('{titleslug}/{id}', 'StoreController@index');
+    Route::get('/', 'StoreController@map');
+});
+
+// Cupons 
+Route::group(['prefix' => 'cupons'], function(){
+    Route::get('{store_id}/{id}', 'OfferController@index');
+    Route::get('/', 'OfferController@new_offers');
+});
+
+// Search
 Route::get('/search', 'SearchController@index');
+
+//Sitemap
 Route::get('/sitemap.xml', 'SitemapController@index');
